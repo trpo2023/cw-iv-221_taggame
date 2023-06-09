0ec7baff52b3c43737feb8b2d9a9c1dedfdfc5fa
Semyon Kozlov <semen8406@gmail.com>
2023-06-09 21:10:47 +0300

    Minor fix after add __init__.py

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,8 +1,6 @@
-from random import randint
-
 class GameLogic:
-    ##def init(self):
-    ##    pass
+    def init(self):
+        pass
 
     def from_rgb(self, r, g, b):
         return str(f'#{r:02x}{g:02x}{b:02x}')
