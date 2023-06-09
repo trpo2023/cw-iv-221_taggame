6b62d18378553ab3c6308ad31389d30053d5c3ed
Semyon Kozlov <semen8406@gmail.com>
2023-06-09 20:55:38 +0300

    #4 Remove mixed function from game_logic.py

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,21 +1,8 @@
 from random import randint
 
 class GameLogic:
-    def init(self):
-        pass
+    ##def init(self):
+    ##    pass
 
     def from_rgb(self, r, g, b):
         return str(f'#{r:02x}{g:02x}{b:02x}')
-
-
-    def mixed(self, n):
-        for i in range(n ** 2):
-            x1 = randint(0, n - 1)
-            y1 = randint(0, n - 1)
-            x2 = randint(0, n - 1)
-            y2 = randint(0, n - 1)
-            self.Bt[x1][y1], self.Bt[x2][y2] = self.Bt[x2][y2], self.Bt[x1][y1]
-            x = self.Btn[x1][y1]["text"]
-            y = self.Btn[x2][y2]["text"]
-            self.Btn[x1][y1]["text"] = y
-            self.Btn[x2][y2]["text"] = x
