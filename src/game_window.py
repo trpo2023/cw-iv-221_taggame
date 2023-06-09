47174c0407234a5bf69533eb501256ab2d8653ed
Semyon Kozlov <semen8406@gmail.com>
2023-06-09 20:57:27 +0300

    #5 Add mixed function from game_logic.py for better work

--- src/game_window.py
+++ src/game_window.py
@@ -91,6 +91,17 @@ class GameWindow:
 
                     self.proverka()
 
+    def mixed(self, n):
+        for i in range(n ** 2):
+            x1 = randint(0, n - 1)
+            y1 = randint(0, n - 1)
+            x2 = randint(0, n - 1)
+            y2 = randint(0, n - 1)
+            self.Bt[x1][y1], self.Bt[x2][y2] = self.Bt[x2][y2], self.Bt[x1][y1]
+            x = self.Btn[x1][y1]["text"]
+            y = self.Btn[x2][y2]["text"]
+            self.Btn[x1][y1]["text"] = y
+            self.Btn[x2][y2]["text"] = x
 
     def new_game(self, m):
         def func():
