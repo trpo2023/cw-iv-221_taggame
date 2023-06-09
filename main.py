92dfb6315aee76dd46cc263e6d6b42b9f695d720
Semyon Kozlov <semen8406@gmail.com>
2023-06-09 21:08:42 +0300

    #6 Add support game_window.py and game_logic.py for main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from tkinter import Tk
+from src import *
 
 def main():
     root = Tk()
     root.title("ПЯТНАШКИ")
     root.geometry("600x600")
-    ##game = GameWindow(root)
+    game = GameWindow(root)
     root.mainloop()
+
+if __name__ == "__main__":
+    main()
